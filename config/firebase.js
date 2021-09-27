d281df645d0878eee25ea886a4c5463d53ccc9a0
Yassine Elhilali <elhilaliyacin@gmail.com>
2021-09-27 17:38:23 +0100

    first commit

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyAsZFJsto4_PbmIYR9AkFsIxNmryj-43oM",
-  authDomain: "meetups-20566.firebaseapp.com",
-  projectId: "meetups-20566",
-  storageBucket: "meetups-20566.appspot.com",
-  messagingSenderId: "476065644190",
-  appId: "1:476065644190:web:8057df43dbd7115f4e058f",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
